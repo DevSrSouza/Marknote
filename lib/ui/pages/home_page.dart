eed1076b021873504314d6fc49791144b5a7591a
DevSrSouza <devsrsouza@gmail.com>
2019-06-10 13:41:08 -0300

    cards load change and remove note bug fixed * removing future builder for fix floating button not appearing * bottom sheet elevation from 4 to 8 * bottom sheet color from card * fix black screen and removing or copying note * fix duplicated notes when remove and undo couple times * copy and remove now unselect note

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -16,13 +16,22 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+enum _CardListStatus { loaded, not_loaded, fail_load }
+
 class _HomePageState extends State<HomePage> {
 
   final _scaffoldKey = GlobalKey<ScaffoldState>();
   final _scrollController = ScrollController();
-  var _cardsloaded = false;
+  var _cardStatus = _CardListStatus.not_loaded;
 
   List<Note> _cards = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadCardList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,12 +47,14 @@ class _HomePageState extends State<HomePage> {
             )
           ],
         ),
-        floatingActionButton: _selectedNoteIndex == null && _cardsloaded && _cards.isNotEmpty ? FloatingActionButton(
+        floatingActionButton: _selectedNoteIndex == null
+            && _cardStatus == _CardListStatus.loaded
+            && _cards.isNotEmpty ? FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: _createNewCard,
         ) : null,
         bottomSheet: _selectedNoteIndex != null ? _bottomSheet(context) : null,
-      body: _cardsList()
+        body: _body(),
         /*child: DragAndDropList(
             _cards.length,
             scrollController: _scrollController,
@@ -58,7 +69,7 @@ class _HomePageState extends State<HomePage> {
             canBeDraggedTo: (one, two) => true,
             dragElevation: 5.0,
             itemBuilder: (context, index) => NoteCard(_cards[index], key: Key(index.toString() + DateTime.now().millisecondsSinceEpoch.toString()))
-        )*/,
+        )*/
         //onReorder: (int actual, int next) {},
         //children: _cards.map((e) => NoteCard(e, key: Key(DateTime.now().millisecondsSinceEpoch.toString()))).toList(),
         //scrollDirection: Axis.vertical,
@@ -86,6 +97,17 @@ class _HomePageState extends State<HomePage> {
   }
 
   int _selectedNoteIndex = null;
+
+  Widget _body() {
+    switch(_cardStatus) {
+      case _CardListStatus.loaded:
+        return _cardsListView();
+      case _CardListStatus.not_loaded:
+        return _waitingCards();
+      case _CardListStatus.fail_load:
+        return _failLoad();
+    }
+  }
 
   Widget _cardsListView() => _cards.isNotEmpty ? ListView.builder(
     controller: _scrollController,
@@ -124,57 +146,51 @@ class _HomePageState extends State<HomePage> {
     ),
   );
 
-  Widget _cardsList() {
-    if(_cardsloaded) {
-      return _cardsListView();
-    } else {
-      return FutureBuilder<List<Note>>(
-        future: NoteHelper().getAllNotes(),
-        builder: (context, snapshot) {
-          switch(snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(
-                child: Container(
-                  width: 200,
-                  height: 200,
-                  alignment: Alignment.center,
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                    strokeWidth: 5,
-                  ),
-                ),
-              );
-            default:
-              if(snapshot.hasError) return Center(
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 100,
-                  width: 150,
-                  child: RaisedButton(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Reload"), Icon(Icons.refresh)
-                      ],
-                    ),
-                    onPressed: () {
-                      setState(() {
+  Widget _waitingCards() => Center(
+    child: Container(
+      width: 200,
+      height: 200,
+      alignment: Alignment.center,
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        strokeWidth: 5,
+      ),
+    ),
+  );
 
-                      });
-                    },
-                  ),
-                ),
-              );
-              else {
-                _cards = snapshot.data;
-                _cardsloaded = true;
-                return _cardsListView();
-              }
-          }
+  Widget _failLoad() => Center(
+    child: Container(
+      alignment: Alignment.center,
+      height: 100,
+      width: 150,
+      child: RaisedButton(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Reload"), Icon(Icons.refresh)
+          ],
+        ),
+        onPressed: () {
+          setState(() {
+            _cardStatus = _CardListStatus.not_loaded;
+          });
+          _loadCardList();
         },
-      );
-    }
+      ),
+    ),
+  );
+
+  Widget _loadCardList() {
+    NoteHelper().getAllNotes().then((notes) {
+      setState(() {
+        _cards = notes;
+        _cardStatus = _CardListStatus.loaded;
+      });
+    }).catchError((e) {
+      setState(() {
+        _cardStatus = _CardListStatus.fail_load;
+      });
+    });
   }
 
   void _showOptions(BuildContext context, int index) {
@@ -184,7 +200,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _bottomSheet(BuildContext context) => BottomSheet(
-    elevation: 4,
+    elevation: 8,
     onClosing: () { // not working: https://github.com/flutter/flutter/issues/27600
       setState(() {
         _selectedNoteIndex = null;
@@ -192,6 +208,7 @@ class _HomePageState extends State<HomePage> {
     },
     builder: (context) {
       return Container(
+        color: Theme.of(context).cardColor,
         padding: EdgeInsets.all(2),
         child: Row(
           mainAxisSize: MainAxisSize.min,
@@ -208,14 +225,12 @@ class _HomePageState extends State<HomePage> {
             IconButton(
               icon: Icon(Icons.content_copy),
               onPressed: () {
-                Navigator.pop(context);
                 _copyNote(_selectedNoteIndex);
               },
             ),
             IconButton(
               icon: Icon(Icons.delete),
               onPressed: () {
-                Navigator.pop(context);
                 _deleteNote(_selectedNoteIndex);
               },
             ),
@@ -233,9 +248,8 @@ class _HomePageState extends State<HomePage> {
     _lastRemovedIndex = index;
 
     setState(() {
+      _selectedNoteIndex = null;
       _cards.removeAt(index);
-
-      NoteHelper().deleteNote(_lastRemovedNote.id);
     });
 
     final snack = SnackBar(
@@ -246,18 +260,17 @@ class _HomePageState extends State<HomePage> {
           onPressed: () {
             setState(() {
               _cards.insert(_lastRemovedIndex, _lastRemovedNote);
-
-              NoteHelper().newNote(
-                  _lastRemovedNote.source,
-                  color: _lastRemovedNote.color,
-                  createTime: _lastRemovedNote.createTime
-              );
             });
           }
       )
     );
 
-    _scaffoldKey.currentState.showSnackBar(snack);
+    var result = _scaffoldKey.currentState.showSnackBar(snack);
+    result.closed.then((reason) {
+      if(reason != SnackBarClosedReason.action) {
+        NoteHelper().deleteNote(_lastRemovedNote.id);
+      }
+    });
   }
 
   void _copyNote(int index) {
@@ -265,6 +278,7 @@ class _HomePageState extends State<HomePage> {
 
     NoteHelper().newNote(note.source, color: note.color).then((note) {
       setState(() {
+        _selectedNoteIndex = null;
         _cards.add(note);
       });
     });
